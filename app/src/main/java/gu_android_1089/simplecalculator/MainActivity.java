0a267692a5ea8205fbc37f8e90a11933361b4712
KosMicBor <gaijin-dk@mail.ru>
2021-04-14 21:53:12 +0500

    Created new repo for Calc

---
+++ app/src/main/java/gu_android_1089/simplecalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package gu_android_1089.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
